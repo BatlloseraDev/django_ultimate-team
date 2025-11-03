221cb0989393bc02471a2a24c6541771da9673d2
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2025-11-03 12:24:48 +0100

    feat:  carga jugadores
    finalizada el script inicial de carga de jugadores

--- utlimate_team/ultimate_team_app/management/commands/carga_jugadores.py
+++ utlimate_team/ultimate_team_app/management/commands/carga_jugadores.py
@@ -12,4 +12,73 @@ class Command(BaseCommand):
         :param options:
         :return:
         """
-        pass
+        self.stdout.write(self.style.SUCCESS('INICIAMOS LA CARGA DE LOS DATOS'))
+
+        fake = Faker('es_Es')
+
+        if Jugador.objects.exists():
+            self.stdout.write(self.style.WARNING(f'Tiene ya datos'))
+        else:
+            #Porteros
+            for i in range(30):
+                jugador = Jugador(
+                    nombre = fake.name(),
+                    nacionalidad = random.choice(Nacionalidad.objects.all()),
+                    equipo = fake.name(),
+                    posicion_id = Posicion.objects.get(tipo='POR'),
+                    pac = random.randint(1, 99),
+                    sho = random.randint(1, 99),
+                    pas = random.randint(1, 99),
+                    dri = random.randint(1, 99),
+                    def_ = random.randint(1, 99),
+                    phy = random.randint(1, 99)
+                )
+                jugador.save()
+
+            #Defensas
+            for i in range(40):
+                jugador = Jugador(
+                    nombre=fake.name(),
+                    nacionalidad=random.choice(Nacionalidad.objects.all()),
+                    equipo=fake.name(),
+                    posicion_id=Posicion.objects.get(tipo='DEF'),
+                    pac=random.randint(1, 99),
+                    sho=random.randint(1, 99),
+                    pas=random.randint(1, 99),
+                    dri=random.randint(1, 99),
+                    def_=random.randint(1, 99),
+                    phy=random.randint(1, 99)
+                )
+                jugador.save()
+            #CentroCampistas
+            for i in range(40):
+                jugador = Jugador(
+                    nombre=fake.name(),
+                    nacionalidad=random.choice(Nacionalidad.objects.all()),
+                    equipo=fake.name(),
+                    posicion_id=Posicion.objects.get(tipo='CEN'),
+                    pac=random.randint(1, 99),
+                    sho=random.randint(1, 99),
+                    pas=random.randint(1, 99),
+                    dri=random.randint(1, 99),
+                    def_=random.randint(1, 99),
+                    phy=random.randint(1, 99)
+                )
+                jugador.save()
+            #Delanteros
+            for i in range(40):
+                jugador = Jugador(
+                    nombre=fake.name(),
+                    nacionalidad=random.choice(Nacionalidad.objects.all()),
+                    equipo=fake.name(),
+                    posicion_id=Posicion.objects.get(tipo='DEL'),
+                    pac=random.randint(1, 99),
+                    sho=random.randint(1, 99),
+                    pas=random.randint(1, 99),
+                    dri=random.randint(1, 99),
+                    def_=random.randint(1, 99),
+                    phy=random.randint(1, 99)
+                )
+                jugador.save()
+            self.stdout.write(self.style.SUCCESS('FIN DE LA CARGA DE LOS DATOS'))
+
